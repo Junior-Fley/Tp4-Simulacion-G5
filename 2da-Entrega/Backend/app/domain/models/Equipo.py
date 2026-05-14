ca64da0e76a4db1769c49f576b9b24b6a558d5ff
Agustín Perez <agustinperez444@gmail.com>
2026-05-14 05:06:11 -0300

    Añadida clase para estados del equipo, añadidas typehints a la clase equipo

--- 2da-Entrega/Backend/app/domain/models/Equipo.py
+++ 2da-Entrega/Backend/app/domain/models/Equipo.py
@@ -1,12 +1,12 @@
-
+from app.domain.models.EstadoEquipo import EstadoEquipo
+from datetime import datetime
 
 class Equipo:
-    def __init__(self, estado, hora_ingreso_taller, horario_inicio_reparacion, horario_fin_reparacion,
-                 tipo_de_reparacion, tiempo_acumulado_reparacion):
-        self.estado = estado
-        self.hora_ingreso_taller = hora_ingreso_taller
-        self.horario_inicio_reparacion = horario_inicio_reparacion
-        self.horario_fin_reparacion = horario_fin_reparacion
-        self.tipo_de_reparacion = tipo_de_reparacion
-        self.tiempo_acumulado_reparacion = tiempo_acumulado_reparacion
-    
+    def __init__(self, estado: EstadoEquipo, hora_ingreso_taller:datetime, horario_inicio_reparacion: datetime,
+                 horario_fin_reparacion: datetime, tipo_de_reparacion: float, tiempo_acumulado_reparacion: float):
+        self.estado: EstadoEquipo = estado
+        self.hora_ingreso_taller: datetime= hora_ingreso_taller
+        self.horario_inicio_reparacion: datetime = horario_inicio_reparacion
+        self.horario_fin_reparacion: datetime = horario_fin_reparacion
+        self.tipo_de_reparacion: float = tipo_de_reparacion
+        self.tiempo_acumulado_reparacion: float = tiempo_acumulado_reparacion
