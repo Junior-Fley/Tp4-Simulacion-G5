c25e39346c26fa906c505cd3af364f8027cfd9d8
Junior Jesus Quispe Ricra <97139@sistemas.frc.utn.edu.ar>
2026-05-18 14:45:44 -0300

    Front: agregue react(con javascript) y axios

---
+++ 2da-Entrega/Frontend/README.md
@@ -0,0 +1,5 @@
+# Para ejecutar  usar el siguiente comanda en la terminar ubicada
+npm run dev
+
+![Imagen de referencia](./imagenReferencia.jpeg)
+
---
+++ 2da-Entrega/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p> Hola mundo </p>
+    </>
+  )
+}
+
+export default App
