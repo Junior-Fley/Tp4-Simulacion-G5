886270e0524ce10fe957a379093822fdf34ee916
Junior Jesus Quispe Ricra <97139@sistemas.frc.utn.edu.ar>
2026-05-23 14:46:20 -0300

    Agregue el CORS en el back

--- 2da-Entrega/Backend/app/infrastructure/start/main.py
+++ 2da-Entrega/Backend/app/infrastructure/start/main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 import uvicorn
 from app.infrastructure.api.controllers.SimularController import router as sim_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI(title="Simulador", version="1.0.0")
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(sim_router)
 
 if __name__ == "__main__":
