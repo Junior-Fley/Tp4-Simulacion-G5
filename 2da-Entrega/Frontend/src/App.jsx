2d3890d1bd16d9a4987e83023cd2dddcd03ac4db
Junior Jesus Quispe Ricra <97139@sistemas.frc.utn.edu.ar>
2026-05-24 13:53:27 -0300

    Elimine el archivo resultados.jsx que ya no lo usaremos

--- 2da-Entrega/Frontend/src/App.jsx
+++ 2da-Entrega/Frontend/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
         <Route path="/" element={<DefVariables />} />
       </Routes>
       <VectorEstado />
-      <Resultados />
       <PiePagina />
       
     </BrowserRouter>
